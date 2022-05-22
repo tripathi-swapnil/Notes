ae8fd39519df11381de4fcb6dd68518c618e94f2
Swapnil Tripathi <nswapt@gmail.com>
2022-05-23 00:26:46 +0530

    Time: 78 ms (83.04%), Space: 46.4 MB (69.73%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function maxDepth(root: TreeNode | null): number {
+    if(root == null) return 0;
+    
+    if(root.left == null && root.right == null ) {
+        return 1;
+    }
+    
+    if(root.left !== null && root.right !== null) {
+        return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+    }
+    
+    if(root.left !== null || root.right !== null) {
+        return 1 + maxDepth(root.left ?? root.right);
+    }
+};
