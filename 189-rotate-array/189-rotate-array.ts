1026a1eb606b58e9d9e7df145a7dad57cf832422
Swapnil Tripathi <nswapt@gmail.com>
2022-05-23 14:52:16 +0530

    Time: 1394 ms (18.19%), Space: 51.7 MB (70.16%) - LeetHub

---
+++ 189-rotate-array/189-rotate-array.ts
@@ -0,0 +1,10 @@
+/**
+ Do not return anything, modify nums in-place instead.
+ */
+function rotate(nums: number[], k: number): void {
+    while(k) {
+        nums.unshift(nums.pop());
+        k--;
+    }
+
+};
