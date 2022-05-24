c27394203bcbef2e37e387375772f64e8fcdaec7
Swapnil Tripathi <nswapt@gmail.com>
2022-05-24 22:49:34 +0530

    Time: 219 ms, Memory: 138.8 MB - LeetHub

---
+++ construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.ts
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+  if (preorder.length == 0) return null;
+  let rootIdxInInorder = getIndex(inorder, preorder[0])
+
+  let left = buildTree(
+    preorder.slice(1, rootIdxInInorder + 1),
+    inorder.slice(0, rootIdxInInorder)
+  );
+
+  let right = buildTree(
+    preorder.slice(rootIdxInInorder + 1),
+    inorder.slice(rootIdxInInorder + 1)
+  );
+  return new TreeNode(preorder[0], left, right);
+}
+
+function getIndex(inorder, val) {
+    let i = 0;
+    while(val !==inorder[i]) {
+          i++;
+    }
+    return i;
+}
