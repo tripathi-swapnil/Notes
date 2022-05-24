2995ac2e34a7ba23b7445ca0e139da78577430a9
Swapnil Tripathi <nswapt@gmail.com>
2022-05-24 13:23:51 +0530

    Time: 92 ms, Memory: 44.5 MB - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.ts
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    let arr = [];
+    function inorder(node){
+        if(!node) return;
+        inorder(node.left);
+        arr.push(node.val);
+        inorder(node.right)
+        
+    }
+    inorder(root);
+    return arr;
+};
