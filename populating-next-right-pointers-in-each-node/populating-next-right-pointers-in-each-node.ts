6e4e5fc1187ad6e923245fe1e96ab9842d93f20e
Swapnil Tripathi <nswapt@gmail.com>
2022-05-24 23:28:03 +0530

    Time: 146 ms, Memory: 50.6 MB - LeetHub

---
+++ populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.ts
@@ -0,0 +1,48 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     left: Node | null
+ *     right: Node | null
+ *     next: Node | null
+ *     constructor(val?: number, left?: Node, right?: Node, next?: Node) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function connect(root: Node | null): Node | null {
+    if(!root) return null;
+    root.next = null;
+  let q = [];
+  let arr = [];
+  
+  
+    q.push(root);
+  while(q.length) {
+      let qLength = q.length;
+      let stk = [];
+      while(qLength > 0) {
+            qLength--;
+            let n = q.shift();
+          if(n) {
+              stk.push(n);
+          }
+            
+             if(n && n.left) {
+                 q.push(n.left);
+             }
+            if(n && n.right) {
+                q.push(n.right);
+             }
+      }
+      for(let c = 0; c < stk.length; c++) {
+          stk[c].next = stk[c+1] || null;
+      }
+        
+  }
+    return root;
+};
