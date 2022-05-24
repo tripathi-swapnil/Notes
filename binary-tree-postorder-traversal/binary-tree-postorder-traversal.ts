c4e3ac7756671ce5360229be655061a1f3bd34c8
Swapnil Tripathi <nswapt@gmail.com>
2022-05-24 13:26:06 +0530

    Time: 105 ms, Memory: 44.8 MB - LeetHub

---
+++ binary-tree-postorder-traversal/binary-tree-postorder-traversal.ts
@@ -0,0 +1,25 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function postorderTraversal(root: TreeNode | null): number[] {
+    let arr = [];
+    function postOrder(node){
+        if(!node) return;
+        postOrder(node.left);
+        postOrder(node.right);
+        arr.push(node.val);
+    }
+    postOrder(root);
+    return arr;
+};
