bab11480f37af9195a6b2cbe95eb11ff4ee65ad5
Swapnil Tripathi <nswapt@gmail.com>
2022-05-24 15:18:46 +0530

    Time: 102 ms, Memory: 44.6 MB - LeetHub

---
+++ symmetric-tree/symmetric-tree.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function isSymmetric(root: TreeNode | null): boolean {
+    if(!root) return false;
+    return checkSymm(root.left, root.right);
+
+};
+
+function checkSymm(left, right) {
+    if(left === null && right === null) return true;
+    
+    if(left === null || right === null) return false;
+    
+    if(left && right && left.val === right.val) {
+        return checkSymm(left.left, right.right) && checkSymm(left.right, right.left);
+    }
+    return false;
+    
+    
+}
