0c3ae5384573eef2822978675ed238de3bc7ed65
Swapnil Tripathi <nswapt@gmail.com>
2022-05-25 10:10:39 +0530

    Time: 179 ms (39.73%), Space: 49.5 MB (35.52%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.ts
@@ -3,7 +3,7 @@ function lengthOfLongestSubstring(s: string): number {
     let j = 0;
     let max = 0;
     for(var i = 0; i < s.length;) {
-        console.log(s[i], {i, j}, s[j] );
+       // console.log(s[i], {i, j}, s[j] );
         if(!subs[s[i]]) {
             subs[s[i]] = 1;
             i++;
