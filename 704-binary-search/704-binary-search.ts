1ffc0443062d4976be541e66b8e87a97e1bbcbec
Swapnil Tripathi <nswapt@gmail.com>
2022-05-25 09:27:12 +0530

    Time: 141 ms (5.13%), Space: 46.9 MB (6.85%) - LeetHub

--- 704-binary-search/704-binary-search.ts
+++ 704-binary-search/704-binary-search.ts
@@ -1,28 +1,22 @@
 function search(nums: number[], target: number): number {
-    return isMatchingItem(nums, 0, nums.length -1 , target);
+    function isMatchingItem(start, end) {
+        if (start > end) return -1;
+
+        const getMid = Math.floor(end+start/2);
+        if(target == nums[getMid]) {
+            return getMid;
+        }
+        if(target < nums[getMid]) {
+            return isMatchingItem(start, getMid -1);
+        }
+
+        if(target > nums[getMid]) {
+            return isMatchingItem(getMid+1, end);
+        }
+        return 0; 
+    }
+    return isMatchingItem(0, nums.length -1);
     
 
 };
 
-function isMatchingItem(nums, start, end, target) {
-    // console.log(start, end, nums);
-    //if(start === end && nums[start] == target) return target;
-    if (start > end) return -1;
-    
-    const getMid = Math.floor(end+start/2);
-    if(target == nums[getMid]) {
-        return getMid;
-    }
-    
-    // console.log({getMid});
-    if(target < nums[getMid]) {
-        return isMatchingItem(nums,start, getMid -1, target);
-    }
-    
-    if(target > nums[getMid]) {
-        return isMatchingItem(nums, getMid+1, end, target);
-    }
-    return 0;
-    
-    
-}
