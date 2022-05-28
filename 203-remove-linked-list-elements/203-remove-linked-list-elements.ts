1f93280897cb141eca73632d26f08bca094ea54a
Swapnil Tripathi <nswapt@gmail.com>
2022-05-28 12:17:01 +0530

    Time: 113 ms (58.20%), Space: 47.4 MB (85.25%) - LeetHub

---
+++ 203-remove-linked-list-elements/203-remove-linked-list-elements.ts
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    // console.log(this.head);
+    let dummy = new ListNode(-1, head);
+    let current = dummy;
+    while(current) {
+        
+        //console.log(current);
+          if(current.next && current.next.val === val) {
+             current.next =  current?.next?.next;
+          } else {
+              current = current.next;
+          }
+        
+    }
+    return dummy.next;
+
+};
