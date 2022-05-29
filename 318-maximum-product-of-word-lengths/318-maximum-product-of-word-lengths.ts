a1d939b544c3a3aa5c5ab21af4d710792548b20e
Swapnil Tripathi <nswapt@gmail.com>
2022-05-29 11:23:02 +0530

    Time: 611 ms (30.00%), Space: 45.2 MB (100.00%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.ts
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.ts
@@ -17,19 +17,19 @@ function maxProduct(words: string[]): number {
 };
 
 function hasNoIntersection(a, b) {
-    let mp = {};
+    // let mp = {};
     for(let  i= 0; i < a.length; i++) {
-        mp[a[i]] = 1;
+        // mp[a[i]] = 1;
+        if(b.indexOf(a[i]) >= 0) {
+            return false;
+        }
+    }
+    // for(let  i= 0; i < b.length; i++) {
+    //     if(mp[b[i]]) return false;
         // if(b.indexOf(a[i]) >= 0) {
         //     return false;
         // }
-    }
-    for(let  i= 0; i < b.length; i++) {
-        if(mp[b[i]]) return false;
-        // if(b.indexOf(a[i]) >= 0) {
-        //     return false;
-        // }
-    }
+    // }
     return true
 }
 
