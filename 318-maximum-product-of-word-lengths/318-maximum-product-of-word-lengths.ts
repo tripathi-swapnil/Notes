cc8976d3e7033ef40fa0bc493ef949b1035832bd
Swapnil Tripathi <nswapt@gmail.com>
2022-05-29 07:17:22 +0530

    Time: 390 ms (30.00%), Space: 45.8 MB (100.00%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.ts
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.ts
@@ -3,7 +3,8 @@ function maxProduct(words: string[]): number {
     let i = 0, j = i + 1;
     while(i < words.length - 1) {
           if(hasNoIntersection(words[i], words[j])) {
-              maxWordLen = Math.max(maxWordLen, words[i].length*words[j].length)
+              const multi = words[i].length*words[j].length;
+              maxWordLen = maxWordLen  > multi ? maxWordLen : multi
           }
         j++;
         if(j >= words.length) {
@@ -16,7 +17,7 @@ function maxProduct(words: string[]): number {
 };
 
 function hasNoIntersection(a, b) {
-    for(let i=0; i < a.length; i++) {
+    for(let  i= 0; i < a.length; i++) {
         if(b.indexOf(a[i]) >= 0) {
             return false;
         }
