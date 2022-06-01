900523d34955f1d71117064eb95694474ffad7fa
Swapnil Tripathi <nswapt@gmail.com>
2022-06-01 08:06:24 +0530

    Time: 140 ms (5.49%), Space: 44.4 MB (84.15%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.ts
@@ -3,6 +3,6 @@ function runningSum(nums: number[]): number[] {
     for(let i = 1; i < nums.length; i++) {
         nums[i] = nums[i] + nums[i-1];
     }
-    console.log(nums);
+    //console.log(nums);
     return nums;
 };
