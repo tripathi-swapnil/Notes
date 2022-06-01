f819d438f5e17bae2ab60eddbaede0b769c945ed
Swapnil Tripathi <nswapt@gmail.com>
2022-06-01 08:29:13 +0530

    Time: 84 ms (64.29%), Space: 44.2 MB (64.29%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.ts
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.ts
@@ -2,12 +2,11 @@ function arrayStringsAreEqual(word1: string[], word2: string[]): boolean {
     let w1 = word1.reduce((acc, v) => {
         acc += v;
         return acc;
-    }, '');//.split('').sort((a, b) => { return a>b ? -1 : 1}).join('');
+    }, '');
     let w2 = word2.reduce((acc, v) => {
         acc += v;
         return acc;
-    }, '');//.split('').sort((a, b) => { return a>b ? -1 : 1}).join('');
-     console.log(w1, w2);
+    }, '');
     return w1 === w2;
 
 };
