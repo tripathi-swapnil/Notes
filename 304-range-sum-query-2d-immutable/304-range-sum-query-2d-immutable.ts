bb40774846db59acbfa37c3ca8747392ed04dc10
Swapnil Tripathi <nswapt@gmail.com>
2022-06-03 07:34:55 +0530

    Time: 7179 ms (5.72%), Space: 67.2 MB (74.29%) - LeetHub

--- 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.ts
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.ts
@@ -6,8 +6,11 @@ class NumMatrix {
 
     sumRegion(row1: number, col1: number, row2: number, col2: number): number {
         let sum = 0;
-        for(let i = col1; i <= col2; i++ ) {
-            for(let j = row1; j <= row2; j++ ) {
+        for(let j = row1; j <= row2; j++ ) {
+            //sum += this.mat[j].slice(col1, col2+1).reduce((a,b) => a+b, 0);
+            //console.log(this.mat[j], this.mat[j].slice(col1, col2+1), this.mat[j].slice(col1, col2+1).reduce((a,b) => a+b, 0), sum)
+            
+            for(let i = col1; i <= col2; i++ ) {
                 sum += this.mat[j][i];
             }
         }
