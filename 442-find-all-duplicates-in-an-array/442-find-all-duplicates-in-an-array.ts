588fddbb95124eed8d2fe6ac7c0542b20eb8ef09
Swapnil Tripathi <nswapt@gmail.com>
2022-06-05 20:53:42 +0530

    Time: 101 ms (96.88%), Space: 51.1 MB (78.13%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.ts
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.ts
@@ -1,12 +1,22 @@
 function findDuplicates(nums: number[]): number[] {
-    const set1 = new Set();
-    const set2 = [];
-    for(let num of nums) {
-        if(set1.has(num)) {
-            set2.push(num)
+    let dups = [];
+    for (let i = 0; i < nums.length; i++) {
+        let index = Math.abs(nums[i]) - 1;
+        if (nums[index] < 0) {
+            dups.push(Math.abs(nums[i]));
         }
-        set1.add(num);
+        nums[index] *= -1;
+        //console.log(nums);
     }
-    return set2;
+    return dups;
+    // const set1 = new Set();
+    // const set2 = [];
+    // for(let num of nums) {
+    //     if(set1.has(num)) {
+    //         set2.push(num)
+    //     }
+    //     set1.add(num);
+    // }
+    // return set2;
 
 };
