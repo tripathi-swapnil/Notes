800fd1b0100991b9bb2b3f4b364debafb1cd92ad
Swapnil Tripathi <nswapt@gmail.com>
2022-06-05 17:22:31 +0530

    Time: 114 ms (86.08%), Space: 62 MB (8.86%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.ts
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.ts
@@ -1,8 +1,16 @@
-function findDisappearedNumbers(nums1: number[]): number[] {
-    const arr = [];
-    const nums = new Set(nums1);
-    for(let i = 1; i <= nums1.length; i++) {
-        if(!nums.has(i)) arr.push(i);
-    }
-    return arr;
+function findDisappearedNumbers(nums: number[]): number[] {
+     const map = new Map();
+      nums.forEach((num, i) => map.set(num, i));
+
+      const result = [];
+      for (let i = 1; i <= nums.length; i++) {
+        if (!map.has(i)) result.push(i);
+      }
+      return result;
+    // const arr = [];
+    // const nums = new Set(nums1);
+    // for(let i = 1; i <= nums1.length; i++) {
+    //     if(!nums.has(i)) arr.push(i);
+    // }
+    // return arr;
 };
