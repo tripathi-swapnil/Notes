9bc23357e10e0a842f4d4af6d77cbc34c7d4d950
Swapnil Tripathi <nswapt@gmail.com>
2022-06-09 17:32:49 +0530

    Time: 139 ms (76.09%), Space: 55.4 MB (91.30%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.ts
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.ts
@@ -1,22 +1,23 @@
-function findCenter(edges: number[][]): number {
-    let frequencey = {};
-    let node = 0;
+function findCenter(e: number[][]): number {
+    return e[0][0] == e[1][0] || e[0][0] == e[1][1] ? e[0][0] : e[0][1];
+//     let frequencey = {};
+//     let node = 0;
     
-    for(let i = 0; i < edges.length;i++) {
-        let [first, second] = edges[i];
-        if(frequencey[first]) {
-            frequencey[first] += 1;
-            node  = first;
-            continue;
-        }
-        frequencey[first] = 1
-        if(frequencey[second]) {
-            frequencey[second] += 1;
-            node  = second;
-            continue;
-        }
-        frequencey[second] = 1;
-    }
-    return node;
+//     for(let i = 0; i < edges.length;i++) {
+//         let [first, second] = edges[i];
+//         if(frequencey[first]) {
+//             frequencey[first] += 1;
+//             node  = first;
+//             continue;
+//         }
+//         frequencey[first] = 1
+//         if(frequencey[second]) {
+//             frequencey[second] += 1;
+//             node  = second;
+//             continue;
+//         }
+//         frequencey[second] = 1;
+//     }
+//     return node;
 
 };
