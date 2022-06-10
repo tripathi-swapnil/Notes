219f26e978963ba7b5cfcd4c8d26bdef3b04c33d
Swapnil Tripathi <nswapt@gmail.com>
2022-06-10 08:37:52 +0530

    Time: 130 ms (5.22%), Space: 44.7 MB (67.87%) - LeetHub

--- 133-clone-graph/133-clone-graph.ts
+++ 133-clone-graph/133-clone-graph.ts
@@ -9,15 +9,78 @@
  *     }
  * }
  */
+function cloneGraph(node: Node | null): Node | null {
+  return cloneGraphByDfs(node);
+}
 
-function cloneGraph(root: Node | null): Node | null {
+function cloneGraphByBfs(node: Node | null): Node | null {
+  if (!node) {
+    return null;
+  }
+
+  const clonedNode = new Node(node.val);
+
+  const visited = new Map<Node, Node>();
+  visited.set(node, clonedNode);
+
+  const queue = [node];
+
+  while (queue.length) {
+    const current = queue.shift()!;
+
+    for (const neighbor of current.neighbors) {
+      if (!visited.has(neighbor)) {
+        const clonedNeighbor = new Node(neighbor.val);
+        visited.set(neighbor, clonedNeighbor);
+        visited.get(current)!.neighbors.push(clonedNeighbor);
+        queue.push(neighbor);
+      } else {
+        visited.get(current)!.neighbors.push(visited.get(neighbor)!);
+      }
+    }
+  }
+
+  return clonedNode;
+}
+
+function cloneGraphByDfs(node: Node | null): Node | null {
+  if (!node) {
+    return null;
+  }
+
+  const clonedNode = new Node(node.val);
+
+  const visited = new Map<Node, Node>();
+  visited.set(node, clonedNode);
+
+  const stack = [node];
+
+  while (stack.length) {
+    const current = stack.pop()!;
+
+    for (const neighbor of current.neighbors) {
+      if (!visited.has(neighbor)) {
+        const clonedNeighbor = new Node(neighbor.val);
+        visited.set(neighbor, clonedNeighbor);
+        visited.get(current)!.neighbors.push(clonedNeighbor);
+        stack.push(neighbor);
+      } else {
+        visited.get(current)!.neighbors.push(visited.get(neighbor)!);
+      }
+    }
+  }
+
+  return clonedNode;
+}
+
+function cloneGraph1(root: Node | null): Node | null {
     if(!root) return null;
+    
     let map = new Map();
     
     function clone(node) {
-        if(map.has(node.val)) {
-            return map.get(node.val);
-        }
+        if(map.has(node.val)) return map.get(node.val);
+        
         map.set(node.val, new Node(node.val));
         
         map.get(node.val).neighbors = node.neighbors.map(clone);
