602917b0705c7bf8d7c2ffae9a0b3dc1247967f0
Swapnil Tripathi <nswapt@gmail.com>
2022-06-15 17:13:28 +0530

    Time: 972 ms (25.20%), Space: 157 MB (54.47%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.ts
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.ts
@@ -1,3 +1,31 @@
+function validPath2(n: number, edges: number[][], source: number, destination: number){
+    let arr = Array(n).fill([]).map(a => []);
+    
+    for(let [s, d] of edges) {
+        arr[s].push(d);
+        arr[d].push(s);
+    }
+    
+    let visited = new Set();
+    let stack = [source];
+    visited.add(source);
+    
+    while(stack.length) {
+        let top = stack[stack.length-1];
+          for(let adj of arr[top]) {
+              if(!visited.has(adj)){
+                  stack.push(adj);
+                 // visited.add(adj);
+              }
+          }
+         let item = stack.pop();
+        visited.add(item);
+        if(item === destination) return true;
+        
+    }
+    return false;
+    
+}
 function validPath(n: number, edges: number[][], source: number, destination: number): boolean {
     let arr = Array(n).fill([]).map(a => []);
     
@@ -19,7 +47,7 @@ function validPath(n: number, edges: number[][], source: number, destination: nu
           for(let adj of arr[item]) {
               if(!visited.has(adj)){
                   queue.push(adj);
-                visited.add(adj);
+                 //visited.add(adj);
               }
           }
     }
@@ -63,3 +91,7 @@ function validPath1(n: number, edges: number[][], source: number, destination: n
 // [[0,1],[0,2],[3,5],[5,4],[4,3]]
 // 0
 // 5
+// 10
+// [[4,3],[1,4],[4,8],[1,7],[6,4],[4,2],[7,4],[4,0],[0,9],[5,4]]
+// 5
+// 9
