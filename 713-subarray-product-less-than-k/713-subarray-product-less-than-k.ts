e00cece11ea76fff992a8d23bc12c4c8563848b2
Swapnil Tripathi <nswapt@gmail.com>
2022-06-18 22:47:33 +0530

    Time: 134 ms (44.00%), Space: 48.3 MB (96.00%) - LeetHub

--- 713-subarray-product-less-than-k/713-subarray-product-less-than-k.ts
+++ 713-subarray-product-less-than-k/713-subarray-product-less-than-k.ts
@@ -1,28 +1,37 @@
 function numSubarrayProductLessThanK(nums: number[], k: number): number {
-    if(!k) return 0;
+    if(k <= 1) return 0;
     let count = 0;
     let prev = 1;
+    let left = 0;
     
-    for(let i = 0; i < nums.length; i++) {
-        if(nums[i] < k){
-            count++;
-            prev = nums[i]
-            //console.log(nums[i], count, prev, '----1');
-        } else {
-            continue;
-        }
+    for(let i = 0; i < nums.length;) {
+        prev *= nums[i];
         
-        for(let j = i+1; j < nums.length; j++) {
-            //if(nums[j] >= k) continue; 
-            if(prev * nums[j] < k) {
-                prev *= nums[j];
-                count++;
-            } else {
-                j = nums.length - 1;
-            }
-            // console.log(nums[i], count, prev);
+        while(prev >= k) {
+            prev /= nums[left]
+            left++;
         }
+        count += i -left +1; 
+        i++;
     }
+    
+//     for(let i = 0; i < nums.length; i++) {
+//         if(nums[i] < k){
+//             count++;
+//             prev = nums[i]
+//         } else {
+//             continue;
+//         }
+        
+//         for(let j = i+1; j < nums.length; j++) {
+//             if(prev * nums[j] < k) {
+//                 prev *= nums[j];
+//                 count++;
+//             } else {
+//                 j = nums.length - 1;
+//             }
+//         }
+//     }
     return count;
 
 };
