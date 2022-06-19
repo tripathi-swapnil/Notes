14cc45ed72d4fdd48d928180280f03a2569315ba
Swapnil Tripathi <nswapt@gmail.com>
2022-06-19 13:49:08 +0530

    Time: 119 ms (11.59%), Space: 45 MB (8.70%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.ts
+++ 709-to-lower-case/709-to-lower-case.ts
@@ -1,3 +1,13 @@
 function toLowerCase(s: string): string {
-    return s.toLowerCase();
+    let difference = 97 - 65;
+    let sts = '';
+    for(let st of s) {
+        if(st.charCodeAt(0) >= 65 && st.charCodeAt(0) <= 90) {
+            sts += String.fromCharCode(st.charCodeAt(0) + difference);
+            continue;
+         }
+        sts += st;
+        
+    }
+    return sts;
 };
