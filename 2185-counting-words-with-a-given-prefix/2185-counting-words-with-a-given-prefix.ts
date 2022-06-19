4d0103e006ea59fde68aa5af80e463cc5e78c8e7
Swapnil Tripathi <nswapt@gmail.com>
2022-06-19 14:03:09 +0530

    Time: 114 ms (22.45%), Space: 44.1 MB (97.96%) - LeetHub

---
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.ts
@@ -0,0 +1,7 @@
+function prefixCount(words: string[], pref: string): number {
+    let count = 0;
+    for(let i = 0 ; i < words.length; i++) {
+        if(words[i].indexOf(pref) === 0) count++;
+    }
+    return count;
+};
