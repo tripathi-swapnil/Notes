8e88a856c38ffa65d2001380f68e3f2c51eac90a
Swapnil Tripathi <nswapt@gmail.com>
2022-06-23 12:15:26 +0530

    Time: 119 ms (31.31%), Space: 42.1 MB (99.62%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.ts
+++ 509-fibonacci-number/509-fibonacci-number.ts
@@ -1,9 +1,20 @@
-function fib(n: number): number {
+function fib1(n: number): number {
     let list = [0,1];
     for(let i = 2; i <= n; i++) {
         list[i] = list[i-2] + list[i-1]
     }
     return list[n];
+}
+function fib(n: number): number {
+    if(!n) return 0;
+    let prev2 = 0;
+    let prev = 1;
+    for(let i = 2; i <= n; i++) {
+        let curr = prev + prev2;
+        prev2 = prev ;
+        prev = curr;
+    }
+    return prev;
 }
 
 
