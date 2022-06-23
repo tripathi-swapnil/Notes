cc29732473e2b2183fffd5b3b110731beb956d1b
Swapnil Tripathi <nswapt@gmail.com>
2022-06-23 13:17:05 +0530

    Time: 72 ms (83.75%), Space: 42.9 MB (56.39%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.ts
+++ 70-climbing-stairs/70-climbing-stairs.ts
@@ -1,7 +1,19 @@
-const memo = {1:1, 2:2};
+//const memo = {1:1, 2:2};
+// function climbStairs1(n: number): number {
+    // if(memo[n]) return memo[n];
+    // memo[n] = climbStairs(n-1) + climbStairs(n-2);
+    // return memo[n];
+// };
+
 function climbStairs(n: number): number {
-    if(memo[n]) return memo[n];
-    memo[n] = climbStairs(n-1) + climbStairs(n-2);
-    return memo[n];
-};
+    if(n === 1) return 1;
+    let prev2 = 1;
+    let prev = 2;
+    for(let i = 2; i < n; i++) {
+        let c = prev + prev2;
+        prev2 = prev;
+        prev = c;
+    }
+    return prev
+}
 
