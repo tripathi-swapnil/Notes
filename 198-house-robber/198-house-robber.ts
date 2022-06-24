e65effb7ca561c5d1a2086986926a368e42af995
Swapnil Tripathi <nswapt@gmail.com>
2022-06-24 11:44:43 +0530

    Time: 81 ms (67.99%), Space: 43 MB (46.44%) - LeetHub

--- 198-house-robber/198-house-robber.ts
+++ 198-house-robber/198-house-robber.ts
@@ -14,12 +14,28 @@ function rob1(nums: number[]): number {
 function rob(nums: number[]): number {
     if(nums.length === 1) return nums[0];
     let prev2 = nums[0];
-    let prev = Math.max(nums[0], nums[1]);
+    let prev = findMax(nums[0], nums[1]);
     for(var i = 2; i < nums.length; i++) {
-        let curr = Math.max(prev2 + nums[i], prev);
+        let curr = findMax(prev2 + nums[i], prev);
         prev2 = prev;
         prev = curr;
     }
     return prev;
+}
+function findMax(a, b)
+{
+    var z, i, max;
+ 
+    // Perform the subtraction
+    z = a - b;
+ 
+    // Right shift and Bitwise AND
+    i = (z >> 31) & 1;
+ 
+    // Find the maximum number
+    max = a - (i * z);
+ 
+    // Return the maximum value
+    return max;
 }
 
