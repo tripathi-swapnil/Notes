581a96e47b07ecdaa6cb3cba80260922dfaa118f
Swapnil Tripathi <nswapt@gmail.com>
2022-06-24 11:39:11 +0530

    Time: 102 ms (35.98%), Space: 42.5 MB (93.31%) - LeetHub

--- 198-house-robber/198-house-robber.ts
+++ 198-house-robber/198-house-robber.ts
@@ -1,4 +1,4 @@
-function rob(nums: number[]): number {
+function rob1(nums: number[]): number {
     let num = [];
     num[0] = nums[0];
     num[1] = Math.max(nums[0], nums[1]);
@@ -11,7 +11,8 @@ function rob(nums: number[]): number {
 };
 
 
-function rob1(nums: number[]): number {
+function rob(nums: number[]): number {
+    if(nums.length === 1) return nums[0];
     let prev2 = nums[0];
     let prev = Math.max(nums[0], nums[1]);
     for(var i = 2; i < nums.length; i++) {
