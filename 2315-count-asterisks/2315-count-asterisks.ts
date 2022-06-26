8512ce755e2539339fdf057999ff75a83cb30274
Swapnil Tripathi <nswapt@gmail.com>
2022-06-26 08:54:18 +0530

    Time: 80 ms (100.00%), Space: 43.3 MB (100.00%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.ts
@@ -0,0 +1,25 @@
+// function countAsterisks(s: string): number {
+//     let c = 0;
+//     const arr = s.split('|');
+//     //console.log(arr);
+//     for(let i = 0; i < arr.length; i++) {
+//         if(i%2 === 0) {
+//             //console.log(i);
+//            const a = arr[i].split('*');
+//            c += a.length - 1;
+//         }
+//     }
+//     return c;
+// };
+function countAsterisks(s: string): number {
+    let isPipe = false;
+    let c = 0;
+    for(let i = 0; i < s.length; i++) {
+        if(s[i] === '|') {
+            isPipe = !isPipe;
+        } else if(!isPipe && s[i] === '*') {
+            c++;
+        }
+    }
+    return c;
+};
