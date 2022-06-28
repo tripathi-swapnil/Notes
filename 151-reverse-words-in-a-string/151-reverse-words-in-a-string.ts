df31114739074d1236bd588a185c99c0dd3379ea
Swapnil Tripathi <nswapt@gmail.com>
2022-06-28 22:18:49 +0530

    Time: 105 ms (48.23%), Space: 45.3 MB (18.44%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.ts
@@ -4,8 +4,8 @@ function reverseWords(s: string): string {
   for (let i = 0; i < s.length; i++) {
     if (s[i] !== ' ') current += s[i];
 
-    if (current.length && (s[i] === ' ' || i === s.length - 1)) {    
-      result = current + (result.length ? ' ' + result : '');
+    if (current !== '' && (s[i] === ' ' || i === s.length - 1)) {    
+      result = current + (result !== '' ? ' ' + result : '');
       current = '';
     };
   }
