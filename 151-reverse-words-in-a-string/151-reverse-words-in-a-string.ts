afd1fe56993474384c9db32976b174157175d1a5
Swapnil Tripathi <nswapt@gmail.com>
2022-06-28 22:18:15 +0530

    Time: 122 ms (21.28%), Space: 46.1 MB (5.67%) - LeetHub

--- 151-reverse-words-in-a-string/151-reverse-words-in-a-string.ts
+++ 151-reverse-words-in-a-string/151-reverse-words-in-a-string.ts
@@ -2,9 +2,9 @@ function reverseWords(s: string): string {
   let result = '', current = '';
 
   for (let i = 0; i < s.length; i++) {
-    if (s.charAt(i) !== ' ') current += s.charAt(i);
+    if (s[i] !== ' ') current += s[i];
 
-    if (current.length && (s.charAt(i) === ' ' || i === s.length - 1)) {    
+    if (current.length && (s[i] === ' ' || i === s.length - 1)) {    
       result = current + (result.length ? ' ' + result : '');
       current = '';
     };
