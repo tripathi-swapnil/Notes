c2426e02d60a594fe0948c469c2faab8c5cc385d
Swapnil Tripathi <nswapt@gmail.com>
2022-06-30 17:20:10 +0530

    Time: 97 ms (48.89%), Space: 44.1 MB (95.56%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.ts
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.ts
@@ -1,10 +1,11 @@
 function findGCD(nums: number[]): number {
     nums = nums.sort((a,b) => a-b);
     
-    for(let i = nums[0]; i >= 2; i--) {
-        if(nums[0]%i === 0 && nums[nums.length-1]%i ===0) {
-            return i;
-        }
-    }
-    return 1;
+    return gcd(nums[0], nums[nums.length-1]);
 };
+function gcd( a,  b)
+{
+    if (a == 0)
+        return b;
+    return gcd(b % a, a);
+}
