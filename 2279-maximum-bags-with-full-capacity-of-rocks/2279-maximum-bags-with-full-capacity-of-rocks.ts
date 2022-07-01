efb82acc8a91d308d24ac202300457b4f72f0e76
Swapnil Tripathi <nswapt@gmail.com>
2022-07-01 08:48:34 +0530

    Time: 295 ms (21.43%), Space: 62.8 MB (32.14%) - LeetHub

---
+++ 2279-maximum-bags-with-full-capacity-of-rocks/2279-maximum-bags-with-full-capacity-of-rocks.ts
@@ -0,0 +1,23 @@
+function maximumBags(capacity: number[], rocks: number[], additionalRocks: number): number {
+    let spaceLeft = [];
+    let fullBags = 0;
+    for(let i = 0; i < capacity.length; i++) {
+        spaceLeft[i] = capacity[i] - rocks[i];
+    }
+    spaceLeft = spaceLeft.sort((a,b) => a-b);
+    //console.log(spaceLeft);
+    for(let space of spaceLeft){
+        
+        if(additionalRocks <= 0 || additionalRocks < space) {
+            continue;
+        }
+        fullBags++;
+        if(space === 0) {
+            
+            continue;
+        }
+        additionalRocks -= space;
+    }
+    return fullBags;
+    
+};
