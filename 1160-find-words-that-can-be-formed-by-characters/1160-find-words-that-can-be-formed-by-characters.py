8210b9d515968809b5160d8d897eb5951ab3a5ba
Swapnil Tripathi <nswapt@gmail.com>
2022-07-06 14:34:18 +0530

    Time: 198 ms (67.60%), Space: 14.6 MB (37.56%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -9,7 +9,6 @@ class Solution:
                 
         count = 0
         for word in words:
-            # print(self.isMatched(word, frequencyMap), word)
             if self.isMatched(word, frequencyMap):
                 count += len(word)
         return count
@@ -24,37 +23,6 @@ class Solution:
             else:
                 frequencyMap[w] = 1
                 
-            # if fMap.get(w, 'na') == 'na':
-            #     return False
-            # print(word, fMap, frequencyMap, w)
-                
             if fMap.get(w, 'na') == 'na' or frequencyMap[w] > fMap[w]:
                 return False
         return True
-                
-
-                
-# class Solution:
-#     def countCharacters(self, words: List[str], chars: str) -> int:
-#         limitF = [0]*26
-#         for ch in chars:
-#           limitF[ord(ch) - 97] += 1
-
-#         count = 0
-#         for word in words:
-#           if self.canBeFormed(word, limitF):
-#             count += len(word)
-
-#         return count
-
-
-#     def canBeFormed(self, word, limitF: List[int]) -> bool:
-#       current = [0]*26
-#       for ch in word:
-#         index = ord(ch) - 97
-#         current[index] += 1
-
-#         if current[index] > limitF[index]:
-#           return False
-#       return True
-        
