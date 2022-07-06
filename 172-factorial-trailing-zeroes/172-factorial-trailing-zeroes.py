162f469c581fa2b598d8c8715bd776d227e1d4a0
Swapnil Tripathi <nswapt@gmail.com>
2022-07-06 14:55:47 +0530

    Time: 4628 ms (10.47%), Space: 24.3 MB (5.07%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -1,10 +1,19 @@
 class Solution:
+    def memoize(func):
+        cache = {}
+        def wrapper(*args, **kwargs):
+            if (str(args), str(kwargs)) not in cache:
+                cache[(str(args), str(kwargs))] = func(*args, **kwargs)
+            return cache[(str(args), str(kwargs))]
+        return wrapper
+
+    @memoize
     def trailingZeroes(self, n: int) -> int:
         fact = str(self.getfactorial(n))
         return len(fact) - len(fact.rstrip('0'))
         
     def getfactorial(self, n:int) -> int:
-        if n <= 0:
+        if n < 2:
             return 1;
         return n * self.getfactorial(n-1)
         
