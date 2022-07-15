da86e81771e0cae9dcc9952f50d220dd83453d51
Swapnil Tripathi <nswapt@gmail.com>
2022-07-15 10:51:05 +0530

    Time: 107 ms (9.94%), Space: 14 MB (30.61%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -0,0 +1,51 @@
+class Solution:
+    def romanToInt(self, st: str) -> int:
+        dict = {
+            'I':{
+                'value': 1,
+                'pattern':['X','V']
+            },
+            'V':{
+                'value': 5,
+                'pattern': []
+            },
+            'X':{
+                'value': 10,
+                'pattern':['L', 'C']
+            },
+            'L':{
+                'value': 50,
+                'pattern':[]
+            },
+            'C':{
+                'value': 100,
+                'pattern':['D','M']
+            },
+            'D':{
+                'value': 500,
+                'pattern':[]
+            },
+            'M':{
+                'value': 1000,
+                'pattern':[]
+            }
+        }
+        sum = 0
+        for i, s in enumerate(st):
+            # print(sum, dict.get(s)['value'], dict.get(s)['pattern'])
+            val = dict.get(s)['value']
+            try:
+                if i >= len(st) -1:
+                    index = -1
+                else:
+                    index = dict.get(s)['pattern'].index(st[i+1])
+            except ValueError:
+                index = -1
+            
+            if index > -1:
+                sum -= val
+            else:
+                sum += val
+        return sum
+        
+        
