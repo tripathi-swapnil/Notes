93e0403843dc6ad7e6c18f00f4d9bb96f7054b0d
Swapnil Tripathi <nswapt@gmail.com>
2022-07-18 13:35:40 +0530

    Time: 40 ms (72.95%), Space: 13.8 MB (70.03%) - LeetHub

---
+++ 20-valid-parentheses/20-valid-parentheses.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        d = {'(':')', '{':'}','[':']'}
+        stack = []
+        for i in s:
+            if i in d:
+                stack.append(i)
+            elif len(stack) == 0 or d.get(stack.pop()) != i:
+                return False
+            
+        return len(stack) == 0
+            
+        
