552c12ef2430c2d14d5ac0ae5a26573026b7f52d
Swapnil Tripathi <nswapt@gmail.com>
2023-01-07 17:23:08 +0530

    Added solution - LeetHub

---
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.py
@@ -0,0 +1,59 @@
+#User function Template for python3
+
+
+class Solution:
+    
+    #Function to detect cycle in a directed graph.
+    def isCyclic(self, V, adj):
+        # code here
+        visited, path = set(), []
+        
+        def dfs(i):
+            visited.add(i)
+            path.append(i)
+            for node in adj[i]:
+                if node not in visited:
+                    if dfs(node):
+                        return True
+                elif node in path:
+                    return True
+                elif node in visited and node in path:
+                    return False
+            
+            path.pop()
+            return False
+            
+        
+        for i in range(V):
+            if i not in visited:
+                if dfs(i):
+                    return True
+        
+        return False
+            
+            
+        
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import sys
+sys.setrecursionlimit(10**6)
+        
+if __name__ == '__main__':
+    t = int(input())
+    for i in range(t):
+        V,E = list(map(int, input().strip().split()))
+        adj = [[] for i in range(V)]
+        for i in range(E):
+            a,b = map(int,input().strip().split())
+            adj[a].append(b)
+        ob = Solution()
+        
+        if ob.isCyclic(V, adj):
+            print(1)
+        else:
+            print(0)
+# } Driver Code Ends
